96a29b46f3371b4e7704adf2c8c6583b3dee51e3
callumrst <callum@rst.dev>
2025-04-09 02:09:59 +0100

    Initial Commit

---
+++ OpenHaldex/OpenHaldexApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenHaldexApp.swift
+//  OpenHaldex
+//
+//  Created by Callum Roulston on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenHaldexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
