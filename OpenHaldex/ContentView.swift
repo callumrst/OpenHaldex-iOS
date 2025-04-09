9fa1a856bb02e5a6cddbde6f09ea03f9665de017
callumrst <callum@roulston.dev>
2025-04-09 04:54:15 +0100

    Initial build, v0.1.0. Very rough UI design for now. WIP

--- OpenHaldex/ContentView.swift
+++ OpenHaldex/ContentView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.lightGray)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 8) {
+                
+                Text("Forbes Automotive")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+                
+                Text("OpenHaldex Controller")
+                    .font(.title2)
+                    .fontWeight(.medium)
+                
+                Text("Current version: v0.1.0 (dev)")
+                    .foregroundColor(Color.white)
+                
+                Image("falogotrans")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal)
+                
+                VStack(spacing: 16) {
+                    Button(action: {
+                        // Action here
+                    }) {
+                        Text("Connect to OpenHaldex")
+                            .foregroundStyle(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(12)
+                    }
+                    
+                    Button(action: {
+                        // Action here
+                    }) {
+                        Text("Disconnect from OpenHaldex")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.red)
+                            .cornerRadius(12)
+                    }
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            
+            }
         }
-        .padding()
     }
 }
 
