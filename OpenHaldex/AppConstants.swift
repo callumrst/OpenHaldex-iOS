e6ae5c2599982249efe61cd29f5f7a491b2e2499
callumrst <callum@roulston.dev>
2025-04-13 04:25:19 +0100

    Add constants

---
+++ OpenHaldex/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  OpenHaldex
+//
+//  Created by Callum Roulston on 13/04/2025.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let version = "0.1.0 (dev)"
+}
